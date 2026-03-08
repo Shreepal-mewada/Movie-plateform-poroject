81cfaa650dd21fba913e141a0b306fc06f2a23c0
Shreepal-mewada <shreepalme@gmail.com>
2026-03-09 01:13:10 +0530

    fix cors for vercel

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -25,12 +25,16 @@ app.use(helmet());
 // CORS Configuration
 const allowedOrigins = [
     process.env.FRONTEND_URL,
+    'https://movie-plateform-poroject.vercel.app',
     'http://localhost:5173',
     'http://localhost:5174',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
     'http://localhost:3000'
 ].filter(Boolean);
+
+
+
 
 app.use(
     cors({
